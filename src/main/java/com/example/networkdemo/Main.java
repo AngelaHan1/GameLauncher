e9de44bfcd14116e01b51714aed95f55632312b5
AngelaHan1 <velocity3838@gmail.com>
2022-05-20 18:35:01 -0700

    rematch reject needs to be resolved still

--- src/main/java/com/example/networkdemo/Main.java
+++ src/main/java/com/example/networkdemo/Main.java
@@ -106,6 +106,19 @@ public class Main extends Application {
                              toServer.writeObject(newMessage);
                              toServer.reset();
                             break;
+                        case REMATCH_REJECT:
+                            //delete from roomlist, and update
+                            String rejectedGame = (String) messageReceived.getData();
+                            System.out.println(rejectedGame + "\n");
+
+                           // roomList.removeFromList(rejectedGame);
+                            roomList.printList();
+
+                            Message updateRoomList = new Message(roomList, HumanTypes.SEND_GAMECHANNEL);
+                            toServer.writeObject(updateRoomList);
+                            toServer.reset();
+
+                            break;
 
                         case JOIN_GAME:  // this message was sent with the room_id player wanna join
                             String room_id = (String) messageReceived.getData();
